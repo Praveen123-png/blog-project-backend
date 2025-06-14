a9a292adf46b1dc7b17e2a7701cc1e553a6cb40a
Praveen123-png <praveenkumar814444@gmail.com>
2025-06-14 13:42:07 +0530

    adding the delete button

--- index.js
+++ index.js
@@ -11,18 +11,37 @@ app.get("/", (req, res) => {
     res.render("index.ejs");
 });
 
+//separate array for each one or
+var titleArray = [];
+var blogsArray = [];
+
+//an array of objects, this is best for both fetching and deleting
+var post = [
+    {title: "firstBlog", content: "hello"},
+    {title: "firstBlog", content: "hello"},
+]
+
 app.get("/blogs", (req, res) => {
     res.render("blogs.ejs");
+    //     , {
+    //     titleHtml: req.body.title,
+    //     blogsHtml: req.body.blogs,
+    // });
+    
 });
 
 app.post("/submit", (req, res) => {
-    let title = req.body.title;
-    let blogs = req.body.blog;
+    var title = req.body.title;
+    var blogs = req.body.blog;
     // console.log(`Title: ${title} \nText: ${blogs}`);
     res.render("blogs.ejs", {
         titleHtml: title,
         blogsHtml: blogs,
     });
+});
+
+app.delete("/delete", (req, res) => {
+
 });
 
 app.listen(port, () => {
